d8594817efdd12f98fb1d93f85410cc9ee4ae383
Nadiia17 <nababiy@gmail.com>
2023-11-12 20:49:20 +0200

    chakra

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@ import { store, persistor } from 'redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const theme = {
   colors: {
+    brand: {
+      50: '#f7fafc', // Світлий відтінок для фону
+      100: '#edf2f7', // Для карток або панелей
+      200: '#e2e8f0', // Для інших елементів фону
+      300: '#cbd5e0', // Темніший для шрифтів або іконок
+      400: '#a0aec0', // Акцентні кольори
+      500: '#718096', // Основний колір тексту
+      600: '#4a5568',
+      700: '#2d3748',
+      800: '#1a202c',
+      900: '#171923',
+    },
+
     errorColor: '#bc4749',
     primaryText: '#22223b', // Основний текст і елементи
     secondaryText: '#4a4e69', // Додатковий текст і кнопки
@@ -21,15 +35,17 @@ const theme = {
 };
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
-          <ThemeProvider theme={theme}>
-            <App />
-            <GlobalStyle />
-          </ThemeProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <ThemeProvider theme={theme}>
+              <App />
+              <GlobalStyle />
+            </ThemeProvider>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
